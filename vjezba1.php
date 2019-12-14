5be274c362eda51802df455a0d7e5d536dd57e05
lejlaidrizovic <58818277+lejlaidrizovic@users.noreply.github.com>
2019-12-14 10:06:25 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $x=10;
+     $y=10;
+     for($i=0; $i<$y; $i++)  {
+        for($j=0; $i<$x; $i++){
+            if($i==0  || $i==$x-1) {
+            echo "X" ; }
+            else {
+                if($j==0 || $j==$y-1)
+    
+                {
+                    echo "X" ;
+                    
+                } else {
+                    echo"H" ;
+                }
+            }
+        } 
+           echo"<br>" ;
+
+     }
+          
+    ?>  
+</body>
+</html>
+
