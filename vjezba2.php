9f1d04091f9819bfdff3ed74ca767d3aff151f0a
lejlaidrizovic <58818277+lejlaidrizovic@users.noreply.github.com>
2019-12-14 10:46:06 +0100

    Add files via upload

---
+++ vjezba2.php
@@ -0,0 +1,20 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php
+    $niz=array(13,"tekst", array(1,2,14),array("jedan"=>1,"dva" =>2), true) ;
+
+
+    print_r($niz);
+    
+    
+    
+    ?>
+    </body>
+    </html>
