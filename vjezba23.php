e468eec64bf5fe6565edb3ec83685fae8c6cc1e2
lejlaidrizovic <58818277+lejlaidrizovic@users.noreply.github.com>
2019-12-22 20:28:23 +0100

    Add files via upload

---
+++ vjezba23.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$r=2 ;
+$PI= 3.14 ;
+
+{
+   $area = $r*$r*$PI ;
+
+
+}
+
+{
+
+$paremeter= 2*$r*$PI ;
+
+
+
+}
+
+echo $area ;
+echo "<br />" ;
+echo $perimeter ;
+
+
+
+
+?>
+</body>
+</html>
