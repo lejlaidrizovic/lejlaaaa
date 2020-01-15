77720a375b29d47ab29158c3a036e013bf4d15d6
lejlaidrizovic <58818277+lejlaidrizovic@users.noreply.github.com>
2020-01-15 17:40:01 +0100

    Add files via upload

---
+++ vjezba5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $matrica= array($Niz1=array(1,2,3),$Niz2=array(2,2,3),$Niz3=array(3,2,3)) ;
+
+     
+
+     echo($matrica) ;
+
+
+
+
+
+    ?>
+
+
+
+
+</body>
+</html>
